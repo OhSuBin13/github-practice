6619e18ef31a01d8e5ab998e4f1d373d506992f7
OhSuBin13 <dhtnqls0103@naver.com>
2025-11-14 15:39:26 +0900

    refactor: Apply pre-commit

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from typing import Union, Optional
 
+
 def add(x: Union[int, float], y: Union[int, float]) -> Optional[Union[int, float]]:
     try:
         result = x + y
     except Exception as e:
         print(e)
         return None
-    
+
     return result
 
+
 if __name__ == "__main__":
-    print(add(5, 3))        # Output: 8
-    print(add(2.5, 4.5))    # Output: 7.0
-    print(add(1, 2.5))      # Output: 3.5
+    print(add(5, 3))  # Output: 8
+    print(add(2.5, 4.5))  # Output: 7.0
+    print(add(1, 2.5))  # Output: 3.5
