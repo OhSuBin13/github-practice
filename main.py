4e3caa89ccb5d2cb449b4fb1340a6a8756cdee13
OhSuBin13 <dhtnqls0103@naver.com>
2025-11-14 10:45:35 +0900

    feat: Add add function
    #1

---
+++ main.py
@@ -0,0 +1,9 @@
+from typing import Union
+
+def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    return x + y
+
+if __name__ == "__main__":
+    print(add(5, 3))        # Output: 8
+    print(add(2.5, 4.5))    # Output: 7.0
+    print(add(1, 2.5))      # Output: 3.5
